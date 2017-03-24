01f28576a6ca8a71f351dd34057b868378bc1385
Andres Oportus <andresoportus@google.com>
2017-03-24 11:21:47 -0700

    sm6150-common: rootdir: Add SchedTune configuration
    Bug: 36367381
    Test: Boot and check stune configuration effect on sysfs
    
    Change-Id: I838c31d164684a5d88550c92284b65bb50f2ed56
    Signed-off-by: Andres Oportus <andresoportus@google.com>
    Signed-off-by: Arian <arian.kulmer@web.de>

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -127,6 +127,11 @@ echo 85 > /sys/devices/system/cpu/cpu6/cpufreq/schedutil/hispeed_load
 echo "0:1248000" > /sys/module/cpu_boost/parameters/input_boost_freq
 echo 40 > /sys/module/cpu_boost/parameters/input_boost_ms
 
+# Configure default schedTune value for foreground/top-app
+echo 1 > /dev/stune/foreground/schedtune.prefer_idle
+echo 10 > /dev/stune/top-app/schedtune.boost
+echo 1 > /dev/stune/top-app/schedtune.prefer_idle
+
 # Set Memory parameters
 configure_memory_parameters
 
